2573f68b62f9fa88469a30626cbc060e6e002fc7
Poulav Bhowmick <bpoulav@gmail.com>
2024-09-12 20:00:00 +0530

    project3

--- hooks/UseContract.jsx
+++ hooks/UseContract.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import { ClubMembership } from '@/utils/constants';
+import { DigitalLocker } from '@/utils/ethereum';
 
 const useContract = () => {
   const [contract, setContract] = useState(null);
@@ -11,7 +11,9 @@ const useContract = () => {
         try {
           const provider = new ethers.providers.Web3Provider(window.ethereum);
           const signer = provider.getSigner();
-          const contractInstance = new ethers.Contract(ClubMembership.address, ClubMembership.abi, signer);
+          const contractInstance = new ethers.Contract(DigitalLocker.address, DigitalLocker.abi, signer);
+          
+          console.log("Contract instance:", contractInstance);
           
           setContract(contractInstance);
         } catch (error) {
@@ -27,3 +29,4 @@ const useContract = () => {
 }
 
 export default useContract;
+
